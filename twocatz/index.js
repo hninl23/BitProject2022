4fc8fdc43cb3e16b82c9b1c320c1343cd5c83317
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-19 17:51:34 -0400

    set up functions that return random cat names from the array

--- twocatz/index.js
+++ twocatz/index.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+    async function getCatPic(){
     const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
         method: 'GET'
     });
@@ -10,12 +12,30 @@ module.exports = async function (context, req) {
     // we need to receive it as a buffer since this is an image we are receiving from the API
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
 
-    base64data = Buffer.from(data).toString('base64')
+    var base64data = Buffer.from(data).toString('base64')
 //put what you want to turn into base64 inside "originaldata"
 //"originaldata" will be encoded in base64.
+    return base64data
+}
+
+    function getName(){
+        var names = ["Shreya, Emily, Fifi, Beau, Evelyn, Julia, Daniel, Fardeen"];
+        var random_value = Math.floor(names.length * Math.random())
+        var resultname = names[random_value]
+        return resultname
+    }
+
+    var firstcat = await getCatPic();
+    var secondcat = await getCatPic();
+    var name1 = getName();
+    var name2 = getName();
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: { base64data }
+        body: {
+            cat1: firstcat,
+            cat2: secondcat,
+            names: [name1, name2]
+        }
+    }
     };
-}
+
