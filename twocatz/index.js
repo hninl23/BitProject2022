dcf54c774a6594a311ce4d855f4c474eba82ca54
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-19 17:01:47 -0400

    create a function that turn base64 url to image

---
+++ twocatz/index.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
+        method: 'GET'
+    });
+    
+    const data = await resp.arrayBuffer()
+    // we need to receive it as a buffer since this is an image we are receiving from the API
+    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+
+    base64data = Buffer.from(data).toString('base64')
+//put what you want to turn into base64 inside "originaldata"
+//"originaldata" will be encoded in base64.
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: { base64data }
+    };
+}
