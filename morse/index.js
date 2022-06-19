4a0a64f944e7ea84738bbab1996556b9ba6372da
Hnin Lwin <h.lwin0440@gmail.com>
2022-06-19 18:55:06 -0400

    function that return morse code from plaintext

---
+++ morse/index.js
@@ -0,0 +1,19 @@
+const morse = require("morse-code-converter");
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    // english meaning english query
+    const english = req.query.plaintext;
+    var code = "";
+
+    if (typeof english === 'undefined' || english === "") {
+        code = "Please enter some text to  convert!"
+    }else {
+        code = morse.textToMorse(english);
+    }
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: code
+    };
+}
